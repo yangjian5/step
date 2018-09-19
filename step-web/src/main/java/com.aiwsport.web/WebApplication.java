6c949d0d24a3ad3b4212bab6ba1b94772e576a7c
yangjian9 <yangjian9@staff.weibo.com>
2018-09-19 17:37:46 +0800

    step

--- step-web/src/main/java/com.aiwsport.web/WebApplication.java
+++ step-web/src/main/java/com.aiwsport.web/WebApplication.java
@@ -1,10 +1,14 @@
 package com.aiwsport.web;
 
+import com.aiwsport.web.interceptor.AccessHandlerInteceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -16,13 +20,13 @@ public class WebApplication {
         SpringApplication.run(WebApplication.class, args);
     }
 
-//    @Configuration
-//    static class WebMvc implements WebMvcConfigurer {
-//        //增加拦截器
-//        @Override
-//        public void addInterceptors(InterceptorRegistry registry) {
-//            registry.addInterceptor(new AccessHandlerInteceptor())    //指定拦截器类
-//                    .addPathPatterns("/api/**");        //指定该类拦截的url
-//        }
-//    }
+    @Configuration
+    static class WebMvc implements WebMvcConfigurer {
+        //增加拦截器
+        @Override
+        public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(new AccessHandlerInteceptor())    //指定拦截器类
+                    .addPathPatterns("/step/**");        //指定该类拦截的url
+        }
+    }
 }
--- step-web/src/main/java/com.aiwsport.web/interceptor/AccessHandlerInteceptor.java
+++ step-web/src/main/java/com.aiwsport.web/interceptor/AccessHandlerInteceptor.java
@@ -1,63 +1,61 @@
-//package com.aiwsport.web.interceptor;
-//
-//import com.aiwsport.core.ConfigServerException;
-//import com.aiwsport.core.ConfigServerExceptionFactor;
-//import com.google.common.collect.Maps;
-//import com.weibo.jerrycore.ConfigServerException;
-//import com.weibo.jerrycore.ConfigServerExceptionFactor;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.util.Map;
-//
-//public class AccessHandlerInteceptor implements HandlerInterceptor {
-//    private static Map<String, String> ips = Maps.newConcurrentMap();
-//
-//    static {
-//        ips.put("*", "");
-//        ips.put("10.77.9.41", "");
-//        ips.put("10.77.29.179", "");
-//        ips.put("10.77.29.180", "");
-//    }
-//
-//    /**
-//     * controller 执行之前调用
-//     */
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-//            throws Exception {
-//        //返回发出请求的IP地址
-//        if (ips.get("*") != null) {
-//            return true;
-//        } else {
-//            String ip = request.getRemoteAddr();
-//            if (ips.get(ip) != null) {
-//                return true;
-//            } else {
-//                throw new ConfigServerException(ConfigServerExceptionFactor.IP_NOT_VISIT_SERVER);
-//            }
-//        }
-//    }
-//
-//    /**
-//     * controller 执行之后，且页面渲染之前调用
-//     */
-//    @Override
-//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-//                           ModelAndView modelAndView) throws Exception {
-//
-//    }
-//
-//    /**
-//     * 页面渲染之后调用，一般用于资源清理操作
-//     */
-//    @Override
-//    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-//            throws Exception {
-//
-//
-//    }
-//
-//}
+package com.aiwsport.web.interceptor;
+
+import com.aiwsport.core.ConfigServerException;
+import com.aiwsport.core.ConfigServerExceptionFactor;
+import com.google.common.collect.Maps;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+public class AccessHandlerInteceptor implements HandlerInterceptor {
+    private static Map<String, String> ips = Maps.newConcurrentMap();
+
+    static {
+        ips.put("*", "");
+        ips.put("10.77.9.41", "");
+        ips.put("10.77.29.179", "");
+        ips.put("10.77.29.180", "");
+    }
+
+    /**
+     * controller 执行之前调用
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        //返回发出请求的IP地址
+        if (ips.get("*") != null) {
+            return true;
+        } else {
+            String ip = request.getRemoteAddr();
+            if (ips.get(ip) != null) {
+                return true;
+            } else {
+                throw new ConfigServerException(ConfigServerExceptionFactor.IP_NOT_VISIT_SERVER);
+            }
+        }
+    }
+
+    /**
+     * controller 执行之后，且页面渲染之前调用
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+
+    }
+
+    /**
+     * 页面渲染之后调用，一般用于资源清理操作
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+
+
+    }
+
+}
