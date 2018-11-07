d275771c2dacf5f4990daeae9f1d6e5fe5e2cac6
yangjian9 <yangjian9@staff.weibo.com>
2018-11-07 20:07:33 +0800

    step

--- step-core/src/main/java/com/aiwsport/core/service/StepService.java
+++ step-core/src/main/java/com/aiwsport/core/service/StepService.java
@@ -76,7 +76,7 @@ public class StepService {
             // 创建用户
             User userNew = new User();
             userNew.setOpenid(openId);
-            userNew.setCoinnum(9);
+            userNew.setCoinnum(0);
             userNew.setAvatarurl(avatarUrl);
             userNew.setCity(city);
             userNew.setGender(gender);
